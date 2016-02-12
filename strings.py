f4504f7e97ea4009b95fc8ec35f3436bed19d161
cbxcube <cubbed@gmail.com>
2016-02-12 18:10:45 +0100

    exercise with strings
    raw input, date, strings

---
+++ strings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from datetime import datetime
+
+name = raw_input("What is your name?")
+department = raw_input("What is your department?")
+company = "floex"
+time = datetime.now()
+
+print "Welcome in %s" % (company)
+print "Nice to meet you %s, " \
+"from %s department." % (name, department)
+print "Now is : %s\n" % (time)
+
+print "date: "
+print time.year
+print time.month
+print time.day
